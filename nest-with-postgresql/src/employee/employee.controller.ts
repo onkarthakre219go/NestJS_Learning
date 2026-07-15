10a46db423b6d80f9760f3e783454dc3092fc3a8
Onkar Thakre <onkarthakre219go>
2026-07-15 23:22:39 +0530

    Learn CRUD using PostgreSQL and TypeORM

--- nest-with-postgresql/src/employee/employee.controller.ts
+++ nest-with-postgresql/src/employee/employee.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Put, Param, Delete } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { Employee } from './employee.entity';
 
@@ -9,5 +9,30 @@ export class EmployeeController {
     @Post()
     async createEmployee(@Body() body: Partial<Employee>): Promise<Employee> {
         return this.employeeService.create(body);
+    }
+
+    @Get()
+    async findAll(): Promise<Employee[]> {
+        return this.employeeService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<Employee> {
+        return this.employeeService.findOne(id);
+    } 
+
+    @Put(':id')
+    async updatedEmployee(
+        @Param('id') id: number,
+        @Body() body: Partial<Employee>,
+    ): Promise<Employee>{
+        return this.employeeService.update(id, body);
+    }
+
+    @Delete(':id')
+    async deleteEmployee(
+        @Param('id') id: number
+    ): Promise<{ message: string }> {
+        return this.employeeService.delete(id);
     }
 }
--- nest-with-postgresql/src/employee/employee.service.ts
+++ nest-with-postgresql/src/employee/employee.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Employee } from './employee.entity';
 import { Repository } from 'typeorm';
@@ -13,5 +13,34 @@ export class EmployeeService {
     async create(employeeData: Partial<Employee>): Promise<Employee> {
         const employee = this.employeeRep.create(employeeData);
         return this.employeeRep.save(employee);
+    }
+
+    async findAll(): Promise<Employee[]> {
+        return this.employeeRep.find();
+    }
+
+    async findOne(id: number): Promise<Employee> {
+        const employee = await this.employeeRep.findOneBy({ id });
+        if(!employee){
+            throw new NotFoundException(`Employee ID ${id} not found`)
+        }
+        return employee;
+    }
+
+    async update(id: number, updatedData: Partial<Employee>): Promise<Employee> {
+        const employee = await this.employeeRep.findOneBy({ id });
+        if(!employee){
+            throw new NotFoundException(`Employee id ${id} not found`);
+        }
+        const updated = Object.assign(employee, updatedData) 
+        return this.employeeRep.save(updated);
+    }
+
+    async delete(id: number): Promise<{ message: string }> {
+        const result = await this.employeeRep.delete(id);
+        if(result.affected === 0){
+            throw new NotFoundException(`Employee with ID ${id} Not found`)
+        }
+        return { message: `Employee with ID ${id} has been deleted sucessfully!`}
     }
 }
